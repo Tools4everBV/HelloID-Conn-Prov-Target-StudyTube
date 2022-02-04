d537d64cf6455235c57187d10c39ac60b758450f
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2022-02-04 16:36:46 +0100

    Update create.ps1

--- create.ps1
+++ create.ps1
@@ -1,72 +1,45 @@
-#######################################################
+#####################################################
 # HelloID-Conn-Prov-Target-StudyTube-Create
 #
-# Version: 1.0.0.0
-#######################################################
-$VerbosePreference = "Continue"
-
+# Version: 1.0.0
+#####################################################
 # Initialize default value's
 $config = $configuration | ConvertFrom-Json
 $p = $person | ConvertFrom-Json
 $success = $false
-$auditLogs = New-Object Collections.Generic.List[PSCustomObject]
+$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
 
-$account = [PSCustomObject]@{
-    user_id         = $p.PersonId
-    employee_number = ''
+# Account mapping
+$account = [ordered]@{
+    user_id         = $p.ExternalId
     email           = $p.Contact.Business.Email
     first_name      = $p.Name.GivenName
     last_name       = $p.Name.FamilyName
-    language        = 'nl_NL'
+    language        = 'nl'
     date_of_birth   = $p.Details.BirthDate
-    company_role    = $CompanyRole
+    company_role    = $p.PrimaryContract.Title.Name
     phone_number    = $p.Contact.Business.Phone.Fixed
-    address         = $p.Contact.Business.Address.Street
-    house_number    = $p.Contact.Business.Address.HouseNumber
-    postal_code     = $p.Contact.Business.Address.PostalCode
-    city            = $p.Contact.Business.Address.Locality
-    place_of_birth  = $p.Details.BirthLocality
-    send_invite     = ''
-    content_licence = ''
+    address         = ''
+    house_number    = ''
+    postal_code     = ''
+    city            = $City
+    place_of_birth  = ''
+    send_invite     = 'false'
+    content_licence = 'false'
+    employee_number = $p.ExternalId
     cost_centre     = ''
-} | ConvertTo-Json
-
-#region Helper Functions
-function Invoke-StudyTubePagedRestMethod {
-    [CmdletBinding()]
-    [OutputType([System.Collections.Generic.List[object]])]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]
-        $TotalItems,
-
-        [Parameter(Mandatory = $true)]
-        [string]
-        $TotalPages,
-
-        [Parameter(Mandatory = $true)]
-        [string]
-        $Url,
-
-        [Parameter(Mandatory = $true)]
-        [System.Collections.IDictionary]
-        $Headers
-    )
-
-    [System.Collections.Generic.List[object]]$dataList = @()
-    $page = 1
-
-    do {
-        $result = Invoke-RestMethod -Uri $Url -Method GET -Headers $headers
-        foreach ($user in $result){
-            $dataList.Add($user)
-        }
-        $page++
-    } until (($page -gt $TotalPages) -or ($dataList.Count -gt ($TotalItems - 1)) )
-
-    Write-Output $dataList
 }
 
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+switch ($($config.IsDebug)) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
+
+#region functions
 function Resolve-HTTPError {
     [CmdletBinding()]
     param (
@@ -76,113 +49,124 @@ function Resolve-HTTPError {
         [object]$ErrorObject
     )
     process {
-        $HttpErrorObj = @{
+        $httpErrorObj = [PSCustomObject]@{
             FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
             MyCommand             = $ErrorObject.InvocationInfo.MyCommand
             RequestUri            = $ErrorObject.TargetObject.RequestUri
+            ScriptStackTrace      = $ErrorObject.ScriptStackTrace
+            ErrorMessage          = ''
         }
         if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
-            $HttpErrorObj['ErrorMessage'] = $ErrorObject.ErrorDetails.Message
+            $httpErrorObj.ErrorMessage = $ErrorObject.ErrorDetails.Message
         } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
-            $stream = $ErrorObject.Exception.Response.GetResponseStream()
-            $stream.Position = 0
-            $streamReader = New-Object System.IO.StreamReader $Stream
-            $errorResponse = $StreamReader.ReadToEnd()
-            $HttpErrorObj['ErrorMessage'] = $errorResponse
+            $httpErrorObj.ErrorMessage = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
         }
-        Write-Output "'$($HttpErrorObj.ErrorMessage)', TargetObject: '$($HttpErrorObj.RequestUri), InvocationCommand: '$($HttpErrorObj.MyCommand)"
+        Write-Output $httpErrorObj
     }
 }
 #endregion
 
-if (-not($dryRun -eq $true)) {
-    try {
-        Write-Verbose "Creating account for '$($p.DisplayName)'"
+# Begin
+try {
+    Write-Verbose 'Adding authorization headers'
+    $authorization = "$($config.CompanyID):$($config.ApiToken)"
+    $base64String = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($authorization))
+    $headers = [System.Collections.Generic.Dictionary[string, string]]::new()
+    $headers.Add("Authorization", "Basic $($base64String)")
 
-        if ($($config.IsConnectionTls12)) {
-            Write-Verbose 'Switching to TLS 1.2'
-            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
-        }
-
-        Write-Verbose 'Adding Authorization headers'
-        $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($($config.ApiToken))
-        $apiTokenString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
-        $authorization = "$($config.CompanyID):$($apiTokenString)"
-        $base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($authorization))
-
-        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-        $headers.Add("Authorization", "Bearer $base64")
-
-        Write-Verbose "Checking if account for '$($p.DisplayName)' already exists"
-        Write-Verbose 'Retrieving total items in StudyTube'
-        $splatGetTotalItems = @{
-            Uri     = "$($config.baseUrl)/v1/users"
-            Method  = 'HEAD'
-            Headers = $headers
-        }
-        $responseHeaders = Invoke-RestMethod @splatGetTotalItems
-        $totalItems = $totalResults.xTotal = $responseHeaders.Headers.'X-Total'
-        $totalPages = $totalResults.xTotalPages = $responseHeaders.Headers.'X-Total-Pages'
-
-        Write-Verbose 'Retrieving paged results'
-        $splatGetAllUsers = @{
-            Uri        = "$($config.baseUrl)/v1/users"
-            TotalItems = $totalItems
-            TotalPages = $totalPages
-            Headers    = $headers
-        }
-        $allUsers = Invoke-StudyTubePagedRestMethod @splatGetAllUsers
-        $lookup = $allUsers | Group-Object -Property employee_number
-        $checkIfUserExists = $lookup[$account.employee_number]
-        if ($checkIfUserExists){
-            $accountReference = $checkIfUserExists.id
-            $logMessage = "Account for user '$($p.DisplayName)' found. Correlation id: '$accountReference'"
-            Write-Verbose $logMessage
-            $success = $true
-            $auditLogs.Add([PSCustomObject]@{
-                Message = $logMessage
-                IsError = $False
-            })
-        } else {
-            Write-Verbose "Account for user '$($p.DisplayName)' does not exist, proceeding with creating account"
-            $splatParams = @{
-                Uri      = "$($config.BaseUrl)/v1/users"
-                Headers  = $headers
-                Body     = $account | ConvertTo-Json
-                Method   = 'POST'
-            }
-            $results = Invoke-RestMethod @splatParams
-
-            $accountReference = $results.id
-            $logMessage = "Account for '$($p.DisplayName)' successfully created. Correlation id: '$accountReference'"
-            Write-Verbose $logMessage
-            $success = $true
-            $auditLogs.Add([PSCustomObject]@{
-                Message = $logMessage
-                IsError = $False
-            })
-        }
-    } catch {
-        $ex = $PSItem
-        if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
-            $errorMessage = Resolve-HTTPError -Error $ex
-            $auditMessage = "Account for '$($p.DisplayName)' not created. Error: $errorMessage"
-        } else {
-            $auditMessage = "Account for '$($p.DisplayName)' not created. Error: $($ex.Exception.Message)"
-        }
-        $auditLogs.Add([PSCustomObject]@{
-            Message = $auditMessage
-            IsError = $true
-        })
-        Write-Error $auditMessage
+    Write-Verbose 'Retrieving total pages for the user resource'
+    $splatResourceTotalParams = @{
+        Uri     = "$($config.BaseUrl)/v1/users"
+        Method  = 'HEAD'
+        Headers = $headers
     }
-}
+    $null = Invoke-RestMethod @splatResourceTotalParams -ResponseHeadersVariable responseHeaders
 
-$result = [PSCustomObject]@{
-    Success          = $success
-    Account          = $account
-    AccountReference = $accountReference
-    AuditLogs        = $auditLogs
-}
+    Write-Verbose 'Retrieving all users from StudyTube'
+    $page = 0
+    $totalPages = $responseHeaders.'X-Total-Pages'[0] -as [int]
+    $userList = [System.Collections.Generic.List[Object]]::new()
+    do {
+        $splatGetUserParams = @{
+            Uri         = "$($config.BaseUrl)/v1/users?page=$page"
+            Method      = 'GET'
+            Headers     = $headers
+            ContentType = 'application/json'
+        }
+        $userResult = Invoke-RestMethod @splatGetUserParams
+        $userList.AddRange($userResult)
+        $page++
+    } until ($page -eq $totalPages)
 
-Write-Output $result | ConvertTo-Json -Depth 10
+    Write-Verbose "Verify if StudyTube account for: [$($p.DisplayName)] must be created or correlated"
+    $lookupUser = $userList | Group-Object -Property user_id -AsHashTable -AsString
+    $studyTubeUser = $lookupUser[$account.user_id]
+
+    if ($studyTubeUser){
+        $action = 'Correlate'
+    } else {
+        $action = 'Create'
+    }
+
+    # Add an auditMessage showing what will happen during enforcement
+    if ($dryRun -eq $true){
+        $auditLogs.Add([PSCustomObject]@{
+            Message = "$action StudyTube account for: [$($p.DisplayName)], will be executed during enforcement"
+        })
+    }
+
+    # Process
+    if (-not($dryRun -eq $true)){
+        switch ($action) {
+            'Create' {
+                Write-Verbose "Creating StudyTube account for: [$($p.DisplayName)]"
+                $splatCreateUserParams = @{
+                    Uri         = "$($config.BaseUrl)/v1/users"
+                    Method      = 'POST'
+                    Headers     = $headers
+                    ContentType = 'application/json'
+                    Body        = $account | ConvertTo-Json
+                }
+                $createUserResponse = Invoke-RestMethod @splatCreateUserParams
+                $accountReference = $createUserResponse.id
+                break
+            }
+
+            'Correlate'{
+                Write-Verbose "Correlating StudyTube account for: [$($p.DisplayName)]"
+                $accountReference = $studyTubeUser.id
+                break
+            }
+        }
+
+        $success = $true
+        $auditLogs.Add([PSCustomObject]@{
+            Message = "$action account for: [$($p.DisplayName)] was successful. accountReference is: [$accountReference]"
+            IsError = $false
+        })
+    }
+} catch {
+    $success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
+        $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-HTTPError -ErrorObject $ex
+        $errorMessage = "Could not $action StudyTube account for: [$($p.DisplayName)]. Error: $($errorObj.ErrorMessage)"
+    } else {
+        $errorMessage = "Could not $action StudyTube account for: [$($p.DisplayName)]. Error: $($ex.Exception.Message)"
+    }
+    Write-Verbose $errorMessage
+    $auditLogs.Add([PSCustomObject]@{
+        Message = $errorMessage
+        IsError = $true
+    })
+# End
+} finally {
+   $result = [PSCustomObject]@{
+        Success          = $success
+        AccountReference = $accountReference
+        Auditlogs        = $auditLogs
+        Account          = $account
+    }
+    Write-Output $result | ConvertTo-Json -Depth 10
+}
